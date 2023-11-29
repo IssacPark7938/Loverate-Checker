5c6fda7a76494e84c748dc102cea93b92499e11b
IssacPark7938 <134672182+IssacPark7938@users.noreply.github.com>
2023-11-30 12:01:50 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+document.querySelector("button").addEventListener("click", function() {
+    var name1 = document.getElementById("name1").value;
+    var name2 = document.getElementById("name2").value;
+
+    if (name1 === "" || name2 === "") {
+        alert("Please insert name/names");
+        return;
+    }
+    
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    name1 = capitalizeFirstLetter(name1);
+    name2 = capitalizeFirstLetter(name2);
+
+    var lovePercentage = Math.max(Math.random() * 100, Math.random() * 100);
+    lovePercentage = Math.floor(lovePercentage) + 1;
+
+    document.querySelector("h1").innerHTML= "The Loverate between " + name1 + " and " + name2 + " is " + lovePercentage + "%!";
+    console.log("The Loverate between " + name1 + " and " + name2 + " is " + lovePercentage + "%!");
+
+    document.getElementById("name1").value = "";
+    document.getElementById("name2").value = "";
+});
